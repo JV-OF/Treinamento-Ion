164842726e3d94f87d994dfaa12fab17fe48d98f
joao vitor <joaovitorof@outlook.com>
2023-01-25 14:35:40 -0300

    Adicionei os aquivos Views/Tarefas/Index.cshtml. Atualizei os arquivos TarefasController, TarefaViewModel e _Layout(em shared)

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Web.Models;
 
 namespace Tarefas.Web.Controllers;
 
@@ -8,6 +9,17 @@ public class TarefasController : Controller
     public IActionResult Create()
     {
         return View();
+    }
+
+    public IActionResult Index()
+    {
+        var listaDeTarefas = new List<TarefaViewModel>()
+        {
+            new TarefaViewModel() { Titulo = "Escovar os Dentes", Descricao = "Todas as manhãs"},
+            new TarefaViewModel() { Titulo = "Arrumar a cama", Descricao = "Todas as manhãs"},
+            new TarefaViewModel() { Titulo = "Tirar o lixo", Descricao = "Segunda, quarta e sexta"},
+        };
+        return View(listaDeTarefas);
     }
 
 }
